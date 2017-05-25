01cba8eed5ddddf73845cc97be0cb727a9ab3f93
tiago.lucas <tiago.lucas@UF065730>
2017-05-25 12:17:48 -0300

    versão 1

---
+++ src/java/teste/ExemploMB.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author tiago.lucas
+ */
+@ManagedBean
+@RequestScoped
+public class ExemploMB {
+
+    /**
+     * Creates a new instance of ExemploMB
+     */
+    public ExemploMB() {
+    }
+    private String sexo;
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    
+}
